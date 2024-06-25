ddb9e4e360a3c7d08ac73a40ce16d2c0749c7bdf
karenpeter08 <karenp5338@gmail.com>
2024-06-25 09:44:04 -0700

    Initial Commit

---
+++ GitHub/GitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubApp.swift
+//  GitHub
+//
+//  Created by Scholar on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
